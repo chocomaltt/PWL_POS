fca73c3ff737ca488b79fcf21f4fe458537af810
Reza Arya Wijaya <wijayarezaarya@gmail.com>
2024-03-05 09:26:13 +0700

    (Praktikum 1 : $fillable) Mod : Models, Controllers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,17 +9,36 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index(){
-        $data = [
-            'nama' => 'Pelanggan Pertama',
-        ];
-        UserModel::where('username', 'customer-1')->update($data);
         // $data = [
-        //     'username' => 'customer-1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
+        //     'nama' => 'Pelanggan Pertama',
         // ];
-        // UserModel::insert($data);
+        // UserModel::where('username', 'customer-1')->update($data);
+        // // $data = [
+        // //     'username' => 'customer-1',
+        // //     'nama' => 'Pelanggan',
+        // //     'password' => Hash::make('12345'),
+        // //     'level_id' => 4
+        // // ];
+        // // UserModel::insert($data);
+
+        // $user = UserModel::all();
+        // return view('user', ['data'=>$user]);
+
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_dua',
+        //     'nama' => 'Manager 2',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
+            
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345')
+        ];
+        UserModel::create($data);
 
         $user = UserModel::all();
         return view('user', ['data'=>$user]);
