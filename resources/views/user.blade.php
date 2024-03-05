f9e66ac92b6cfbd3109cee3090819995d9395980
Reza Arya Wijaya <wijayarezaarya@gmail.com>
2024-03-05 11:39:40 +0700

    (Praktikum 2.4 : Retrieving or Creating Models) Mod : Controllers, Models, Views

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -11,21 +11,21 @@
     <h1>Data User</h1>
     <table border="1" cellpadding="2" cellspacing="0">
         <tr>
-            {{-- <th>ID</th>
+            <th>ID</th>
             <th>Usernam</th>
             <th>Nama</th>
-            <th>ID Level Pengguna</th> --}}
-            <th>Jumlah Pengguna</th>
+            <th>ID Level Pengguna</th>
+            {{-- <th>Jumlah Pengguna</th> --}}
         </tr>
         {{-- @foreach($data as $d) --}}
         <tr>
-            {{-- <td>{{ $data->user_id}}</td>
+            <td>{{ $data->user_id}}</td>
             <td>{{ $data->username}}</td>
             <td>{{ $data->nama}}</td>
-            <td>{{ $data->level_id}}</td> --}}
-            
+            <td>{{ $data->level_id}}</td>
+
             {{-- Untuk Menampilkan Hasil Dari Count() --}}
-            <td>{{ $data }}</td> 
+            {{-- <td>{{ $data }}</td>  --}}
         </tr>
         {{-- @endforeach --}}
     </table>
