ef682281a83698d1604ab154a57b71f1ccbe9a93
Reza Arya Wijaya <wijayarezaarya@gmail.com>
2024-03-26 05:15:15 +0700

    Tugas Praktikum - Soal No.2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -54,8 +54,8 @@
  
  
 @push('js') 
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <script src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script> 
- 
 @endpush 
  
 @stack('scripts') 
@@ -64,6 +64,7 @@
 {{-- Add common CSS customizations --}} 
  
 @push('css') 
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 <link 	rel="stylesheet" 
 href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.css" />  
 <style type="text/css"> 
